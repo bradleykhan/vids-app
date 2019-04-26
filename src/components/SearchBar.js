bfa45780bdff21e56f1e44e0a2704aa078642654
Bradley Khan <github@bradleykhan.com>
2019-04-26 17:10:38 +0100

    Apply styling and set up state for Search Bar component

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,8 +1,39 @@
 import React from "react";
 
 class SearchBar extends React.Component {
+    constructor() {
+        super();
+
+        this.state = { term: "" };
+
+        this.onInputChange = this.onInputChange.bind(this);
+    }
+
+    onFormSubmit(event) {
+        event.preventDefault();
+
+        //Make sure we call callback from parent component
+    }
+
+    onInputChange(event) {
+        this.setState({ term: event.target.value });
+    }
+
     render() {
-        return <div>Search bar</div>;
+        return (
+            <div className="search-bar ui segment">
+                <form onSubmit={this.onFormSubmit} className="ui form">
+                    <div className="field">
+                        <label>Video Search</label>
+                        <input
+                            type="text"
+                            value={this.state.term}
+                            onChange={this.onInputChange}
+                        />
+                    </div>
+                </form>
+            </div>
+        );
     }
 }
 
